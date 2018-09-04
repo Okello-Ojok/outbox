5928aeab244a5df45c3df7dd83cb37e2d1fe8681
Okello Emmanuel <emmanuel.alvino.okello06@gmail.com>
2018-09-04 21:10:40 +0300

    UPDATE: cleaning the head

--- routes/index.js
+++ routes/index.js
@@ -13,11 +13,6 @@ const Services = mongoose.model('Services');
 const Payments = mongoose.model('Payments');
 const Branding = mongoose.model('Branding');
 
-
-
-
-
-
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
